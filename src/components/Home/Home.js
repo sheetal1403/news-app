e0d1549f7afe6d314def34635421e6db78d89ce7
Sheetal R <sheetalraghupathi14@gmail.com>
2020-09-30 16:38:32 +0530

    display fetched items

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import Card from '../Card/Card';
 
 class Home extends Component{
 
+    state = {
+        items: [],
+        loading: true
+    }
+
     componentDidMount(){
+        
         axios.get('/reports.json')
-            .then(response => console.log(response.data.items))
+            .then(response =>{
+                this.setState({
+                    loading: false,
+                    items: response.data.items
+                })
+            })
             .catch(e => console.log(e))
     }
 
     render(){
+
+        let items = <p>Loading</p>;
+        if(!this.state.loading){
+            items = this.state.items.map(item => <Card headline = {item.item.headline[0]} key={item.id}/>)
+        }
+
         return (
             <div>
-                HOME
+               {items} 
             </div>
         )
     }
